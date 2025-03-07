35f204bb89c971e3d4b6f3f25f3b912cda642094
Melissa Andrade Garcia Da Silva <meandrad@c1r7p1.42sp.org.br>
2025-03-06 21:50:01 -0300

    refactor: add push_swap to srcs folder

---
+++ srcs/push_swap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 13:53:14 by meandrad          #+#    #+#             */
+/*   Updated: 2025/03/06 20:08:32 by meandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || argc == 2 && argv[1][0] == '\0')
+		return (1);
+	init_stack_a(&a, argv + 1);
+}
