3752da65b41afd9e23b187de9a501388f4b8c103
Melissa Andrade Garcia Da Silva <meandrad@c2r12p2.42sp.org.br>
2025-03-12 13:40:51 -0300

    ajustando funções no .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 21:56:21 by meandrad          #+#    #+#             */
-/*   Updated: 2025/03/06 21:56:24 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/03/11 20:37:50 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,13 @@ typedef struct s_stack_node
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;
 }	t_stack_node;
+
+void	init_stack_a(t_stack_node *a, char *argv[]);
+int		ft_stack_len(t_stack_node *stack);
+
+void	print_error(t_stack_node **a);
+void	free_stack(t_stack_node **a);
+int		error_input(char *s);
+int		handle_duplicate(t_stack_node *a, int n);
 
 #endif
