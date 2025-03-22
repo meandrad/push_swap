34021da57620729582d4f47d1ecde3aaebcf73a4
Melissa Andrade Garcia Da Silva <meandrad@c3r1p3.42sp.org.br>
2025-03-22 17:15:23 -0300

    Nova pasta de comandos de sort

---
+++ srcs/instructions/reverse_rotate_stack.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate_stack.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 23:04:14 by meandrad          #+#    #+#             */
+/*   Updated: 2025/03/22 15:46:29 by meandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	reverse_rotate(t_stack_node **head)
+{
+	t_stack_node	*tail;
+
+	if (!*head || !(*head)->next)
+		return ;
+	tail = search_last_node(*head);
+	tail->prev->next = NULL;
+	tail->next = (*head);
+	tail->prev = NULL;
+	(*head) = tail;
+	tail->next->prev = tail;
+}
+
+void	rra(t_stack_node **stack_a)
+{
+	reverse_rotate(stack_a);
+	ft_putstr("rra");
+	ft_putchar("\n");
+}
+
+void	rrb(t_stack_node **stack_b)
+{
+	reverse_rotate(stack_b);
+	ft_putstr("rrb");
+	ft_putchar("\n");
+}
+
+void	rrr(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+}
---
+++ srcs/instructions/rotate_stack.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_stack.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 18:51:20 by meandrad          #+#    #+#             */
+/*   Updated: 2025/03/22 14:38:49 by meandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	rotate_stack(t_stack_node **head)
+{
+	t_stack_node	*tail;
+
+	if (!*head || !(*head)->next)
+		return ;
+	tail = search_last_node(*head);
+	tail->next = *head;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	tail->next->prev = tail;
+	tail->next->next = NULL;
+}
+
+void	ra(t_stack_node **stack_a)
+{
+	rotate_stack(stack_a);
+	ft_putstr("ra");
+	ft_putchar("\n");
+}
+
+void	rb(t_stack_node **stack_b)
+{
+	rotate_stack(stack_b);
+	ft_putstr("ra");
+	ft_putchar("\n");
+}
+
+void	rr(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	rotate_stack(stack_a);
+	rotate_stack(stack_b);
+}
