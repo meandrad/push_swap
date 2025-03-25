b5e4f6426c9f3060f52335f2a289e687d8d7b0c0
Melissa Andrade Garcia Da Silva <meandrad@c3r12p1.42sp.org.br>
2025-03-25 19:43:14 -0300

    Adicionando funções no arquivo .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 21:56:21 by meandrad          #+#    #+#             */
-/*   Updated: 2025/03/18 20:04:21 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/03/25 19:42:01 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,32 @@ typedef struct s_stack_node
 	struct s_stack_node	*prev;
 }	t_stack_node;
 
-void	start_stack_a(t_stack_node *a, char *argv[]);
-int		ft_stack_len(t_stack_node *stack);
-
-void	print_error(t_stack_node **a);
-void	free_stack(t_stack_node **a);
+// ./
 int		error_input(char *s);
+int		ft_stack_len(t_stack_node *stack);
 int		handle_duplicate(t_stack_node *a, int n);
+int		main(int argc, char *argv[]);
+void	add_node(t_stack_node **a, int n);
+void	free_stack(t_stack_node **a);
+void	print_error(t_stack_node **a);
+void	start_stack_a(t_stack_node *a, char *argv[]);
+
+// instructions/
+void	pa(t_stack_node **stack_a, t_stack_node **stack_b);
+void	pb(t_stack_node **stack_a, t_stack_node **stack_b);
+void	push_stack(t_stack_node **stack1, t_stack_node **stack2);
+void	ra(t_stack_node **stack_a);
+void	rb(t_stack_node **stack_b);
+void	reverse_rotate(t_stack_node **head);
+void	rotate_stack(t_stack_node **head);
+void	rr(t_stack_node **stack_a, t_stack_node **stack_b);
+void	rra(t_stack_node **stack_a);
+void	rrb(t_stack_node **stack_b);
+void	rrr(t_stack_node **stack_a, t_stack_node **stack_b);
+void	sa(t_stack_node **stack_a);
+void	sb(t_stack_node **stack_b);
+void	sort_tree_nodes(t_stack_node **stack);
+void	ss(t_stack_node **stack_a, t_stack_node **stack_b);
+void	swap_stack(t_stack_node **head);
 
 #endif
