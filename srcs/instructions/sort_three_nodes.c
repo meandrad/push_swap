3a8cea9a711a1361b2bae8dfc9416e587e25a540
Melissa Andrade Garcia Da Silva <meandrad@c3r12p1.42sp.org.br>
2025-03-25 21:30:20 -0300

    Atualização do nome da função

---
+++ srcs/instructions/sort_three_nodes.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three_nodes.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 17:18:46 by meandrad          #+#    #+#             */
+/*   Updated: 2025/03/25 21:29:36 by meandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+void	sort_three_nodes(t_stack_node **stack)
+{
+	t_stack_node	*biggest_node;
+
+	biggest_node = search_max(*stack);
+	if (biggest_node == *stack)
+		ra(*stack);
+	else if ((*stack)->next == biggest_node)
+		rra(*stack);
+	if ((*stack)->nbr > (*stack)->next->nbr)
+		sa(*stack);
+}
