8d6658bdaa93a0376caf5b997ac6b48081f3f80c
Melissa Andrade Garcia Da Silva <meandrad@c3r13p5.42sp.org.br>
2025-03-30 15:28:54 -0300

    ajustando erros de norma

--- srcs/instructions/sort_stack.c
+++ srcs/instructions/sort_stack.c
@@ -6,26 +6,26 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 13:19:06 by meandrad          #+#    #+#             */
-/*   Updated: 2025/03/30 15:14:37 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/03/30 15:28:29 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
 
-static void	double_rotation(t_stack_node **stack_a, t_stack_node **stack_b, t_stack_node *cheapest)
+static void	double_rr(t_stack_node **a, t_stack_node **b, t_stack_node *cheap)
 {
-	while (*stack_b != cheapest->target_node && *stack_a != cheapest)
-		rr(stack_a, stack_b);
-	set_index(*stack_a);
-	set_index(*stack_b);
+	while (*b != cheap->target_node && *a != cheap)
+		rr(a, b);
+	set_index(*a);
+	set_index(*b);
 }
 
-static void	double_reverse_rotate(t_stack_node **stack_a, t_stack_node **stack_b, t_stack_node *cheapest)
+static void	double_rrr(t_stack_node **a, t_stack_node **b, t_stack_node *cheap)
 {
-	while (*stack_b != cheapest->target_node && *stack_a != cheapest)
-		rrr(*stack_a, *stack_b);
-	set_index(*stack_a);
-	set_index(*stack_b);
+	while (*b != cheap->target_node && *a != cheap)
+		rrr(*a, *b);
+	set_index(*a);
+	set_index(*b);
 }
 
 static void	send_a_to_b(t_stack_node **stack_a, t_stack_node **stack_b)
@@ -35,27 +35,18 @@ static void	send_a_to_b(t_stack_node **stack_a, t_stack_node **stack_b)
 	cheapest = search_cheapest(*stack_a);
 	if (cheapest->above_mediam && cheapest->target_node->above_mediam)
 		double_rotation(*stack_a, *stack_b, cheapest);
-	else if (!(cheapest->above_mediam) && !(cheapest->target_node->above_mediam))
+	else if (!(cheapest->above_mediam)
+		&& !(cheapest->target_node->above_mediam))
 		double_reverse_rotate(*stack_a, *stack_b, cheapest);
 	prep_for_push(stack_a, cheapest, 'a');
 	prep_for_push(stack_b, cheapest->target_node, 'b');
 	pb(stack_b, stack_a);
 }
+
 static void	send_b_to_a(t_stack_node **stack_a, t_stack_node **stack_b)
 {
 	prep_for_push(stack_a, (*stack_b)->target_node, 'a');
 	pa(stack_a, stack_b);
-}
-
-static void	put_min_top(t_stack_node **stack_a)
-{
-	while ((*stack_a)->nbr != search_min(*stack_a)->nbr)
-	{
-		if (search_min(*stack_a)->above_mediam)
-			ra(stack_a);
-		else
-			rra(stack_a);
-	}
 }
 
 void	sort(t_stack_node **stack_a, t_stack_node **stack_b)
